24a625598827c7d20b4104c42e83ef10c2185393
pyoki32 <pyoki32@gmail.com>
2024-07-14 16:03:06 +0900

    JPA와 DB 설정, 동작확인

--- src/test/java/sideprj/sidejpa/MemberRepositoryTest.java
+++ src/test/java/sideprj/sidejpa/MemberRepositoryTest.java
@@ -1,18 +1,18 @@
 package sideprj.sidejpa;
 
-
 import org.assertj.core.api.Assertions;
-
-
 import org.junit.Test;
+
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.junit.Assert.*;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringRunner.class )
 @SpringBootTest
 public class MemberRepositoryTest {
 
@@ -20,19 +20,19 @@ public class MemberRepositoryTest {
 
     @Test
     @Transactional
-    public void testMember() throws Exception{
+    public void testMemberRepository() throws Exception {
         //given
         Member member = new Member();
-        member.setUsername("memberA");
+        member.setUsername("member A");
 
         //when
         Long saveId = memberRepository.save(member);
         Member findMember = memberRepository.find(saveId);
 
-        //than
+        //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember == member: " + (findMember == member));
     }
-
 }
